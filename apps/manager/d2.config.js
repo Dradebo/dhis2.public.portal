639e93a7a15b73692b1800f90b1368e7e2d70cb4
Dradebo <dradebosean@gmail.com>
2025-07-17 12:19:44 +0300

    removed explicit versioning

--- apps/manager/d2.config.js
+++ apps/manager/d2.config.js
@@ -6,8 +6,6 @@ const config = {
 	entryPoints: {
 		app: "./src/App.tsx",
 	},
-	minDHIS2Version: "2.40",
-	maxDHIS2Version: "2.42",
 };
 
 module.exports = config;
--- apps/manager/src/App.tsx
+++ apps/manager/src/App.tsx
@@ -9,6 +9,7 @@ import {
 import { routeTree } from "./routes.gen";
 import { DialogProvider } from "@hisptz/dhis2-ui";
 import "leaflet/dist/leaflet.css";
+import { Provider } from "@dhis2/app-runtime";
 
 const hashHistory = createHashHistory();
 
@@ -22,9 +23,16 @@ declare module "@tanstack/react-router" {
 	}
 }
 
+const dhis2Config = {
+	baseUrl: process.env.REACT_APP_DHIS2_BASE_URL || "/api",
+	apiVersion: process.env.REACT_APP_DHIS2_API_VERSION || "39",
+};
+
 const MyApp = () => (
+	<Provider config={dhis2Config}>
 		<DialogProvider>
 			<RouterProvider router={router} />
 		</DialogProvider>
+	</Provider>
 );
 export default MyApp;
